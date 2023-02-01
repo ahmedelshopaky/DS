615adc373528b022082f555c9dcc3c7939540774
ahabdelfatah <ahabdelfatah22@mans.edu.eg>
2023-02-01 17:14:14 +0200

    Recursion

--- Stack/main.c
+++ Stack/main.c
@@ -6,49 +6,83 @@ void Display(StackEntry e)
     printf("e is: %d\n", e);
 }
 
-int main()
+void TestStack()
 {
     StackEntry e;
     Stack s;
-    int x;
     CreateStack(&s);
-
     if (!StackFull(&s))
     {
         Push(100, &s);
+        Push(200, &s);
+        Push(300, &s);
     }
+    printf("stack size is: %d\n", StackSize(&s));
+    TraverseStack(&s, &Display);
     if (!StackEmpty(&s))
     {
         StackTop(&e, &s);
-        printf("e is: %d\n", e);
+        printf("stack entry is: %d\n", e);
         Pop(&e, &s);
+        printf("stack entry is: %d\n", e);
+        Pop(&e, &s);
+        printf("stack entry is: %d\n", e);
     }
-    x = StackSize(&s);
-    printf("x is: %d\n", x);
+    printf("stack size is: %d\n", StackSize(&s));
     ClearStack(&s);
-    TraverseStack(&s, &Display);
-    return 0;
+    printf("stack size is: %d\n", StackSize(&s));
 }
 
-// void ReverseRead(void)
-// {
-//     StackEntry item;
-//     Stack stack;
-//     CreateStack(&stack);
-//     while (!StackFull(&stack) && (item = getchar()) != '\n')
-//         Push(item, &stack);
-//     while (!StackEmpty(&stack))
-//     {
-//         Pop(&item, &stack);
-//         putchar(item);
-//     }
-//     putchar('\n');
-// }
+void ReverseRead()
+{
+    StackEntry item;
+    Stack stack;
+    CreateStack(&stack);
+    while (!StackFull(&stack) && (item = getchar()) != '\n')
+        Push(item, &stack);
+    while (!StackEmpty(&stack))
+    {
+        Pop(&item, &stack);
+        putchar(item);
+    }
+    putchar('\n');
+}
+
+/*
+ * Stacks applications: Recursion
+ * Towers of Hanoi
+ * Pre: There is at least (count) disks on the tower (start).
+ * The top disk on each of towers (temp) and (finish) is
+ * larger than any of the top (count) disks on tower (start).
+ * Post: The top (count) disks on (start) have been moved to (finish),
+ * (temp) has been returned to its starting position.
+ */
+void MoveDisks(int count, int start, int finish, int temp)
+{
+    /*
+     * No. of function calls = 2^count - 1
+     * This step of printf is the base condition.
+     */
+    if (count > 0)
+    {
+        MoveDisks(count - 1, start, temp, finish);
+        printf("Move disk %d from %d to %d\n", count, start, finish);
+        MoveDisks(count - 1, temp, finish, start);
+    }
+}
+
+int main()
+{
+    MoveDisks(64, 1, 3, 2);
+    // TestStack();
+    // ReverseRead();
+    return 0;
+}
 
 /*
  * Information Hiding (Encapsulation)
  * WHAT - HOW
- * I am in the user level, I don't care about details at the implementation level
+ * In the user level, I don't care about details at the implementation level
  *
  * Top-Down-Design !!!
  * The execution time, complexity, depends on n.!!!
